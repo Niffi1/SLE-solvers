e82a1aab87e50e2d6b45a9f1b04d3977507ac9a7
Stan <sroma@users.noreply.github.com>
2017-10-05 22:19:58 +1000

    Update bicg_stab.f90

--- bicg_stab.f90
+++ bicg_stab.f90
@@ -1,9 +1,8 @@
-! Biconjugate gradient stabilized method
-! COPMPILATION: f90 linalg.f90 bicg_stab.f90 -o bicg_stab.exe -xopenmp
+! COPMPILATION: f90 linalg.f90 main.f90 -o bicg.exe -xopenmp
 ! RUN         : ./bicg.exe Ndim NumProc -key
-!                               Ndim - matrix dimension; NumProc - number of openmp threads
-!                               key: -3diag - test run for 3 diag matrix
-!                                        -identity - test run for E matrix
+!				Ndim - matrix dimension; NumProc - number of openmp threads
+!				key: -3diag - test run for 3 diag matrix
+!					 -identity - test run for E matrix
 
 program bicg_stab
 use linalg
@@ -16,8 +15,7 @@ integer :: i,j,k,it
 real*8, dimension(:,:), allocatable :: A
 real*8, dimension(:), allocatable :: b, x, s, t, r, p, v
 real*8 :: alpha, beta, rho0, rho1, omega, bb, rr
-
-real*8, parameter :: eps = 1.0E-5
+real*8, parameter :: eps = 1.e-9
 real*8 :: t1, t2, t3
 
 ! command arguments
@@ -39,6 +37,8 @@ t1 = omp_get_wtime()
 allocate(A(n,n))
 A = 0.
 allocate(b(n))
+allocate(x(n))
+x = 0.
 b = 0.
  
  call omp_set_num_threads(NUMPROC)
@@ -61,21 +61,27 @@ select case (trim(mode))
          enddo
        A(1:2,1) = (/-3.0,1.0/); A(N-1:N,N) = (/1.0,-3.0/)
        b(1) = -10.; b(n) = -10.
-        case default
-          print *, "Try -identity or -3diag flags. Run for bad nonsymmetric matrix."
-          A = 1.333
-          A(1,:n/3) = 100.
-          A(n,:n/3) = -10.
-          b = n * 1.333
-          b(1) = 0.1
-          b(n) = -133.901
+	case default
+	  print *, "Try -idenitity or -3diag flags. Run for bad nonsymmetric matrix."
+	  do i = 1, n
+		x(i) = (i-1)*9/11.
+		do j =1, n
+		  A(i,j) = 1.333 * (i-1) + j-1
+		enddo
+		A(i, i) = (i-1) * (i-1)
+	  enddo
+
+	  do i = 1, n
+		b(i) = 0.
+		do j = 1, n
+		  b(i) = b(i) + A(i,j) * x(j)
+		enddo
+	  enddo
 end select
 
 t2 = omp_get_wtime()
-
 allocate(s(n))
 allocate(t(n))
-allocate(x(n))
 allocate(r(n))
 allocate(p(n))
 allocate(v(n))
@@ -87,24 +93,23 @@ rho0 = 1.
 alpha = 1.
 omega = 1.
 bb = norm2(b)
-if (bb < 1.e-10) bb = 1.
 rr = bb
 
-! BiCGSTAB: let r0 = b
+! BiCG: let r0 = b
 it = 0
 do while (rr / bb > eps)
-        rho1 = dotprod(b,r)
-        beta = rho1 * alpha / (rho0 * omega)
-        p = r + beta * (p - omega * v)
-        v = multmv(A,p)
-        alpha = rho1 / dotprod(b, v)
-        s = r - alpha * v
-        t = multmv(A,s)
-        omega = dotprod(t,s) / dotprod(t,t)
-        x = x + alpha * p + omega * s
-        r = s - omega * t
-        rr = norm2(r)
-        rho0 = rho1
+	rho1 = dotprod(b,r)
+	beta = rho1 * alpha / (rho0 * omega)
+	p = r + beta * (p - omega * v)
+	v = multmv(A,p)
+	alpha = rho1 / dotprod(b, v)
+	s = r - alpha * v
+	t = multmv(A,s)
+	omega = dotprod(t,s) / dotprod(t,t)
+	x = x + alpha * p + omega * s
+	r = s - omega * t
+	rr = norm2(r)
+	rho0 = rho1
     it = it + 1
     if (mod(it,250) == 0) print"('DEBUG ||r||2/||b||2 = ',ES10.2, ' on ',i5 ' iters.')", rr / bb, it
 enddo
@@ -113,7 +118,7 @@ t3 = omp_get_wtime()
 print "('||r||2/||b||2 = ',ES10.2, ' on ',i5 ' iters.')", rr / bb, it
 print '("Method time ",f10.5,"s for ",i7,"-size on ", i3 " threads.")', t3 - t2, N, NUMPROC
 print '("Initial time, s: ",f10.5)',t2 - t1
-print '("CHECK of solve (method): ",L)',check_solve(a,x,b,eps)
+print '("CHECK of solve (method): ",L)',check_solve(A,x,b,1.e-6_8)
 if(N <= 10)print *,"x: ", x
 
 deallocate(A)
